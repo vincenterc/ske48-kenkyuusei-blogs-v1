99c102933f4ebf1e30ae3aa48a37d90d36d6863f
cms <cms@test.com>
2017-05-12 03:40:09 +0800

    Updating blogs when app starts

--- app/lib/updateBlogsAtIntervals/updateBlogsAtIntervals.js
+++ app/lib/updateBlogsAtIntervals/updateBlogsAtIntervals.js
@@ -1,10 +1,15 @@
 const updateBlogsInDB = require('./updateBlogsInDB')
 
+const updateBlogs = (mongodbUri) => {
+  const time = new Date()
+  console.log(time.toLocaleString())
+  updateBlogsInDB(mongodbUri)
+}
+
 const updateBlogsAtIntervals = (mongodbUri, interval) => {
+  updateBlogs(mongodbUri)
   setInterval(() => {
-    const time = new Date()
-    console.log(time.toLocaleString())
-    updateBlogsInDB(mongodbUri)
+    updateBlogs(mongodbUri)
   }, interval * 1000)
 }
 
