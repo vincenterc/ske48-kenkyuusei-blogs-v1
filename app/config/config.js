b31fb59724ec7664667570e7887a20d69c1d54ee
cms <cms@test.com>
2017-06-06 07:07:25 +0800

    change env variable 'MLAB_SKEB48LOGS_PASSWORD' to 'MLAB_SKE48BLOGS_PASSWORD'

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 3001
 const dbUsername = process.env.MLAB_SKE48BLOGS_USERNAME
-const dbPassword = process.env.MLAB_SKEB48LOGS_PASSWORD
+const dbPassword = process.env.MLAB_SKE48BLOGS_PASSWORD
 const dbUrl = process.env.MLAB_SKE48BLOGS_URL || 'localhost:27017/ske48blogs'
 const mongodbUri = dbUsername ? `mongodb://${dbUsername}:${dbPassword}@${dbUrl}`
                               : `mongodb://${dbUrl}`
