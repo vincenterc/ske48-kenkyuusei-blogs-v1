dbe26583a7cf890494f16cb6a21de31c4d41cc25
cms <cms@test.com>
2017-06-06 17:54:58 +0800

    change the variable name, 'posts' to 'post', in the file 'insertPostToDB' to make more sense

--- app/lib/updateBlogsAtIntervals/insertPostToDB.js
+++ app/lib/updateBlogsAtIntervals/insertPostToDB.js
@@ -3,12 +3,12 @@ const MongoClient = require('mongodb').MongoClient
 // insert the document: {writer: <String>, name: <String>, generation: <Number>
 //                       posts: [{id: <String>, date: <String>, title: <String>}, ...]}
 const insertPostToDB = async (mongodbUri, collection,
-                              writer, name, generation, posts) => {
+                              writer, name, generation, post) => {
   const db = await MongoClient.connect(mongodbUri)
   await db.collection(collection).update(
     { writer },
     { $set: { name, generation },
-      $push: { posts } },
+      $push: { posts: post } },
     { upsert: true })
   db.close()
 }
